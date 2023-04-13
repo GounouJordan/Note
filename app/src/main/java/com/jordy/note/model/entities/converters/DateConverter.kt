04240e8b8af5c730936107968506b60cfd2eda9c
jordan gounou <gounoujordan@gmail.com>
2023-04-13 13:09:59 +0100

    Create DateConverter for room entities

---
+++ app/src/main/java/com/jordy/note/model/entities/converters/DateConverter.kt
@@ -0,0 +1,18 @@
+package com.jordy.note.model.entities.converters
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import java.util.*
+
+@ProvidedTypeConverter
+class DateConverter {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
