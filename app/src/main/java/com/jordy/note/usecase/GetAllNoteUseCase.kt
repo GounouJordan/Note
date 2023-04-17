fd33779d79563209809bf351465d72224467fbe6
jordan gounou <gounoujordan@gmail.com>
2023-04-17 12:54:41 +0100

    modified GetAllNoteUseCase.kt to use Pager instead of Flow

--- app/src/main/java/com/jordy/note/usecase/GetAllNoteUseCase.kt
+++ app/src/main/java/com/jordy/note/usecase/GetAllNoteUseCase.kt
@@ -2,25 +2,16 @@ package com.jordy.note.usecase
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.map
-import com.jordy.note.model.data.Note
-import com.jordy.note.model.data.noteFromNoteEntity
+import com.jordy.note.model.entities.NoteEntity
 import com.jordy.note.repository.NoteRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 
 class GetAllNoteUseCase(
     private val noteRepository: NoteRepository
 ) {
-    operator fun invoke(pageSize: Int = 15): Flow<PagingData<Note>> =
+    operator fun invoke(pageSize: Int = 15): Pager<Int, NoteEntity> =
         Pager(
             config = PagingConfig(pageSize = pageSize)
         ) {
             noteRepository.getAll()
-        }.flow.map { pagingData ->
-            pagingData.map {
-                noteFromNoteEntity(it)
-            }
         }
 }
